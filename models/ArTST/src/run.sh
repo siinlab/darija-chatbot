4ca7c03b89b6b7b85691061559f51830afa6cc63
Ayoub <contact@siinlab.com>
2024-11-22 00:17:18 +0100

    create run.sh script to prepare data

---
+++ models/ArTST/src/run.sh
@@ -0,0 +1,36 @@
+set -e
+
+# Go to the directory of this script
+cd "$(dirname "$0")"
+
+src_dir=$(pwd)
+
+# Go to the root directory of the project
+cd ../../..
+
+# Go to dataset directory
+cd dataset
+
+# iterate over all the folders
+for folder in */; do
+    # Go to the folder
+    cd $folder
+
+    # if it doesn't contain a csv file, skip it
+    ls *.csv || continue
+
+    # Get the csv file
+    csv_file=$(ls *.csv)
+
+    # get audio folder name
+    audios_dir=$(ls -d */)
+
+    # Run the Python script
+    python $src_dir/data.py \
+        --csv_path "$csv_file" \
+        --audios_dir "$audios_dir" \
+        --manifest_path "./manifest.tsv"
+
+    # Go back to the dataset directory
+    cd ..
+done

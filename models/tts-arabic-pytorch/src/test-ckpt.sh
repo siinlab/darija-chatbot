7048e331a35a6c78109f0ac6a9c90fb87f86cf6a
Ayoub <contact@siinlab.com>
2024-12-13 12:16:59 +0100

    add script to test model

---
+++ models/tts-arabic-pytorch/src/test-ckpt.sh
@@ -0,0 +1,17 @@
+set -e
+
+# Go to the directory of this script
+cd "$(dirname "$0")"
+
+src_dir=$(pwd)
+
+ckpt_path="$1"
+
+# copy updated script file
+cp "$src_dir/test_raw_model.py" ../tts-arabic-pytorch/
+
+# Go to the directory of the TTS model
+cd ../tts-arabic-pytorch/
+
+# run script
+python test_raw_model.py --ckpt_path "$ckpt_path"
---
+++ models/tts-arabic-pytorch/src/test_raw_model.py
@@ -0,0 +1,28 @@
+import sounddevice as sd
+from models.fastpitch import FastPitch2Wave
+import argparse
+
+def remove_diacrits(text: str): 
+    return ''.join([c for c in text if c not in ['َ', 'ِ', 'ُ' , 'ٍ', 'ٌ', 'ْ' , 'ٰ']])
+
+
+# read ckpt_path using argparse
+parser = argparse.ArgumentParser("Test FastPitch2Wave checkpoint")
+parser.add_argument("--ckpt_path", type=str, required=True)
+args = parser.parse_args()
+ckpt_path = args.ckpt_path
+
+model = FastPitch2Wave(ckpt_path).cuda()
+
+text = "اَلسَّلامُ عَلَيكُم يَا صَدِيقِي."
+text = "أَتَاحَتْ لِلْبَائِعِ المُتَجَوِّلِ أنْ يَكُونَ جَاذِباً لِلمُوَاطِنِ الأقَلِّ دَخْلاً."
+
+wave = model.tts(text, speaker_id=0, phonemize=False)
+
+sd.play(wave, 22050)
+
+text_ = remove_diacrits(text)
+
+wave = model.tts(text_, speaker_id=0, phonemize=False)
+
+sd.play(wave.cpu(), 22050)

314b10707e579de8f3e4bb698db5f50b4560114d
Ayoub <contact@siinlab.com>
2025-01-25 02:23:56 +0100

    update train.sh by adding hyperparams

--- models/whisper-asr/src/train.sh
+++ models/whisper-asr/src/train.sh
@@ -9,3 +9,11 @@ hf_dataset_path="$src_dir/../../../dataset/all-datasets.arrow"
 
 python $src_dir/train.py --data-dir $hf_dataset_path \
  --output-dir $src_dir/checkpoints/ \
+ --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 8 \
+    --learning_rate 1e-4 \
+    --warmup_steps 500 \
+    --max_steps 10000 \
+    --fp16 \
+    --generation_max_length 512 \
+    

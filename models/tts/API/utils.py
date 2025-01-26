59444e4470006e2cd1ddaab19b4f5b2f09475f1b
Ayoub <contact@siinlab.com>
2025-01-26 17:12:31 +0100

    fix import issue

--- models/tts/API/utils.py
+++ models/tts/API/utils.py
@@ -1,5 +1,6 @@
 """Utility functions for the API."""
 
+import os
 from pathlib import Path
 from sys import path
 
@@ -7,10 +8,7 @@ _here = Path(__file__).resolve().parent
 
 
 def append_to_sys_path() -> None:
-	"""Appends specific directories to the system path based on the provided flags.
-
-	Returns:
-		None
-	"""
+	"""Appends specific directories to the system path based on the provided flags."""
 	models_path = (_here.parent / "tts-arabic-pytorch").as_posix()
 	path.insert(0, models_path)
+	os.chdir(models_path)

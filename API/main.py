f2abc5bafa4cd949978da7d67c5910067220ba0a
Ayoub <contact@siinlab.com>
2025-01-26 15:56:45 +0100

    change PATH

--- API/main.py
+++ API/main.py
@@ -1,9 +1,19 @@
 """This module contains the main entry point for the Darija TTS API."""
 
+from os import environ
+from pathlib import Path
+
 from fastapi import FastAPI
 
 # import routers from model's API directories
 from models.whisper_asr.API.main import router as whisper_asr_router
+
+root_dir = Path(__file__).resolve().parent
+
+# Define paths
+environ["PATH"] = environ["PATH"] + ":" + root_dir.as_posix()
+print(environ["PATH"])  # noqa: T201
+
 
 app = FastAPI("Darija TTS API")
 

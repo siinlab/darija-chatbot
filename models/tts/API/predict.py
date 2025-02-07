c9be24af82685684f83ec5d748a9e301c5a9a082
Ayoub <contact@siinlab.com>
2025-02-07 21:39:59 +0100

    add silence in between text parts split by punctuation

--- models/tts/API/predict.py
+++ models/tts/API/predict.py
@@ -73,8 +73,67 @@ def generate_wav(text: str, voice: Voice, checkpoint: str = "states_6000") -> st
 		models[model_name] = model
 	else:
 		model = models[model_name]
-	wav = model.tts(text, speaker_id=0, phonemize=False)
-	# save the wave to a file
+	# Split the text into parts based on delimeters
+	texts, silence_durations = split_text(text)
+	# Generate the wav file
+	waves = model.tts(
+		texts,
+		speaker_id=0,
+		phonemize=False,
+		speed=1,
+		denoise=0.005,
+		pitch_add=0,
+		pitch_mul=1,
+		batch_size=8,
+	)
+	# add silence between parts
+	sample_rate = 22050
+	wav = waves[0]
+	for i in range(1, len(waves)):
+		silence_duration = silence_durations[i - 1]
+		silence = torch.zeros(int(silence_duration / 1000 * sample_rate))
+		wav = torch.cat([wav, silence, waves[i]], dim=0)
+	# save the wav to a file
 	wav_path = generate_path()
-	torchaudio.save(wav_path, wav.unsqueeze(0).cpu(), 22050)
+	torchaudio.save(wav_path, wav.unsqueeze(0).cpu(), sample_rate)
 	return wav_path
+
+
+def split_text(text: str) -> tuple[list[str], list[int]]:
+	"""Split the text into parts based on delimeters.
+
+	Args:
+		text (str): The text to split.
+
+	Returns:
+		tuple: A tuple containing a list of text parts
+			and a list of silence durations in ms.
+	"""
+	delimeters = {
+		".": 200,
+		"،": 100,
+		"?": 200,
+		"؟": 200,
+		"!": 200,
+		"\n": 200,
+	}
+	# Remove redundant spaces
+	text = " ".join(text.split())
+	# Remove redundant delimeters
+	for delim in delimeters:
+		text = delim.join(list(text.split(delim)))
+	# Split the text by delimeters
+	texts = []
+	current_text = ""
+	silence_durations = []
+	for i in range(len(text)):
+		char = text[i]
+		current_text += char
+		if char in delimeters:
+			texts.append(current_text)
+			silence_durations.append(delimeters[char])
+			current_text = ""
+	if current_text:
+		texts.append(current_text)
+		silence_durations.append(0)
+	return texts, silence_durations

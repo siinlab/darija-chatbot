388c5c55fc345ed276ae19b1c8135f441f233e4a
Ayoub <contact@siinlab.com>
2025-02-07 20:18:14 +0100

    add option for default prompt

--- models/chat/API/predict.py
+++ models/chat/API/predict.py
@@ -10,20 +10,20 @@ client = anthropic.Anthropic(
 	api_key=ANTHROPIC_API_KEY,
 )
 
+DEFAUTL_PROMPT = "انا كندوي بالدارجة و بغيت تبقى تجاوبني بها و بغيتك تبقى تجاوبني بلا حروف لاتينية و بلا ارقام:\n"  # noqa: E501
 
-def predict(messages: list) -> str:
+
+def predict(messages: list, prompt: str = DEFAUTL_PROMPT) -> str:
 	"""Respond to messages using the Claude model.
 
 	Args:
 		messages (list): A list of message dictionaries.
+		prompt (str, optional): The prompt to use for the Claude model.
 
 	Returns:
 		str: The response text from the Claude model.
 	"""
-	messages[0]["content"] = (
-		"انا كندوي بالدارجة و بغيت تبقى تجاوبني بها و بغيتك تبقى تجاوبني بلا حروف لاتينية و بلا ارقام:\n"  # noqa: E501
-		+ messages[0]["content"]
-	)
+	messages[0]["content"] = prompt + messages[0]["content"]
 	message = client.messages.create(
 		model="claude-3-5-sonnet-20241022",
 		max_tokens=512,

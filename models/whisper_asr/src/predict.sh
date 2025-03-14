17821e27e2544f7eaf97774d366fe9f66f744945
Ayoub <contact@siinlab.com>
2025-03-14 20:11:44 +0000

    improve predict script

--- models/whisper_asr/src/predict.sh
+++ models/whisper_asr/src/predict.sh
@@ -4,12 +4,18 @@ set -e
 # Go to the directory of this script
 cd "$(dirname "$0")"
 
+# read checkpoint directory
+ckpt_dir=$1
+# get parent of ckpt_dir
+ckpts_dir=$(dirname "$ckpt_dir")
+# copy contents of ckpt_dir to ckpts_dir
+cp "$ckpt_dir"/* "$ckpts_dir"
+
 src_dir=$(pwd)
-model_path="$src_dir/../checkpoints/"
 audios_dir="$src_dir/../../../dataset/.test-dataset/audios/"
 
 python "$src_dir/predict.py" \
-    --model "$model_path" \
+    --model "$ckpts_dir" \
     --audios "$audios_dir" \
-    --num-samples 200 \
-    --output-dir "$src_dir/../results"
+    --num-samples 10 \
+    --output-dir "$src_dir/../prediction-results/"
